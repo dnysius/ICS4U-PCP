f98bc2fd863782dddaf8ff2d174ca8031aad0e4d
dionysius indraatmadja <dionysius.indraatmadja16@ycdsbk12.ca>
2016-04-12 21:23:09 -0400

    Adds GUI and animations
    https://www.youtube.com/watch?v=5-HHywqUEcM&index=15&list=PLu2K_XrV43vBp40aez8XAjPt-MmzLdJnG

--- WepScript.cs
+++ WepScript.cs
@@ -7,9 +7,13 @@ public class wepScript : MonoBehaviour {
         public GameObject hitPar;
         public int damage = 30;
         public int range = 10000;
+        public int ammo = 10;
+        public int clipSize = 10;
+        public int clipCount = 5;
  
         public Animation am;
         public AnimationClip shoot;
+        public AnimationClip reloadA;
  
  
  
@@ -18,25 +22,46 @@ public class wepScript : MonoBehaviour {
                 if (Input.GetMouseButton (0)) {
                         fireShot ();
                 }
+ 
+                if (Input.GetKeyDown (KeyCode.R)) {
+                        reload();
+                }
         }
  
         public void fireShot(){
-                if (!am.IsPlaying (shoot.name)) {
-                        am.CrossFade (shoot.name);
-                        RaycastHit hit;
-                        Ray ray = fpsCam.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2, 0));
+                if (!am.IsPlaying (reloadA.name) && ammo >= 1) {
+                        if(!am.IsPlaying (shoot.name)){
+                                am.CrossFade (shoot.name);
+                                ammo = ammo - 1;
+ 
+                                RaycastHit hit;
+                                Ray ray = fpsCam.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2, 0));
                
-                        if (Physics.Raycast (ray, out hit, range)) {
-                                if (hit.transform.tag == "Player") {
-                                        hit.transform.GetComponent<PhotonView> ().RPC ("applyDamage", PhotonTargets.AllBuffered, damage);
-                                }
-                                GameObject particleClone;
-                                particleClone = PhotonNetwork.Instantiate (hitPar.name, hit.point, Quaternion.LookRotation (hit.normal), 0) as GameObject;
-                                Destroy (particleClone, 2);
-                                Debug.Log (hit.transform.name);
+                                if (Physics.Raycast (ray, out hit, range)) {
+                                        if (hit.transform.tag == "Player") {
+                                                hit.transform.GetComponent<PhotonView> ().RPC ("applyDamage", PhotonTargets.AllBuffered, damage);
+                                        }
+                                        GameObject particleClone;
+                                        particleClone = PhotonNetwork.Instantiate (hitPar.name, hit.point, Quaternion.LookRotation (hit.normal), 0) as GameObject;
+                                        Destroy (particleClone, 2);
+                                        Debug.Log (hit.transform.name);
                        
+                                }
                         }
        
                 }
+        }
+ 
+        public void reload(){
+                if (clipCount >= 1) {
+                        am.CrossFade (reloadA.name);
+                        ammo = clipSize;
+                        clipCount = clipCount - 1;
+                }
+ 
+        }
+ 
+        void OnGUI(){
+                GUI.Box (new Rect(110,10,150,30), "Ammo: " + ammo + "/" + clipSize + "/" + clipCount);
         }
 }
