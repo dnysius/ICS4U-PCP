6542484319441de0c205d356b7e683ad080eeff9
dionysius indraatmadja <dionysius.indraatmadja16@ycdsbk12.ca>
2016-06-16 21:52:07 -0400

    Delete Crosshair.cs

--- Crosshair.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Crosshair : MonoBehaviour {
-
-	void OnGUI(){
-		GUI.Box(new Rect((Screen.width/2) - 5,(Screen.height/2) - 5, 10, 10), "");
-	}
-}
